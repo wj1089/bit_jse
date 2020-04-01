b21535a151202b2c44a3b090fa76720a1efd1636
wj1089 <wj1089@naver.com>
2020-04-01 19:51:26 +0900

    19:50

--- RPS.java
+++ RPS.java
@@ -10,23 +10,55 @@ public class RPS {
 		
 	}
 	static void game() {
-		int num = 0;
+		
 		System.out.println("가위 바위 보게임을 시작합니다.");
 		System.out.println("1.가위, 2.바위, 3.보 ");
 		Scanner scanner = new Scanner(System.in);
-		int expect = scanner.nextInt();
-		System.out.println("사용자의 값은 "+expect+"입니다.");
+		int use = scanner.nextInt();
+		System.out.println("사용자의 값은 "+use+"입니다.");
 		Random ran = new Random();
-		num = ran.nextInt(3)+1;
-			System.out.println("컴퓨터의 값은 "+num+"입니다.");
-		if(expect<num) {
-			System.out.println("컴퓨터 승!!");
-		}else if(expect==num){
-			System.out.println("비겼습니다");
-		}else{
-			System.out.println("사용자 승!!");
+		int com = ran.nextInt(3)+1;
+		System.out.println("컴퓨터의 값은 "+com+"입니다.");
+		String result = "";
+		
+		if(use == com) {
+			result="무승부";
+			System.out.println(result);	
+			}
+		
+		if(use == 1){
+			if(com == 2) {
+				result = "컴퓨터 승리";
+				System.out.println(result);
+				if(com==3) {
+				result="사용자 승리";
+				System.out.println(result);
+				}
+			}
 		}
-		System.out.println();
-		main(null);
+		
+				
+		if(use == 2){
+			if(com == 1) {
+				result = "사용자 승리";
+				System.out.println(result);
+				if(com==3) {
+				result="컴퓨터 승리";
+				System.out.println(result);
+				}
+			}
+		}
+					
+		if(use == 3){
+			if(com == 1) {
+				result = "컴퓨터 승리";
+				System.out.println(result);
+				if(com==3) {
+				result="사용자 승리";
+				System.out.println(result);
+				}
+			}
+		}
 	}
 }
+	
