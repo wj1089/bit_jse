af5e8a740809b1b3602dc6620709c7086399e662
wj1089 <wj1089@naver.com>
2020-04-01 20:12:07 +0900

    20:10

---
+++ RPSIf.java
@@ -0,0 +1,49 @@
+package com.jse.app;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RPSIf {
+
+	public static void main(String[] args) {
+		game();
+		
+	}
+	static void game() {
+		
+		System.out.println("가위바위보 게임");
+		System.out.println("(1)가위   (2)바위   (3)보  ");
+		Scanner scanner = new Scanner(System.in);
+		int use = scanner.nextInt();
+		System.out.println("플레이어의 선택"+ use);
+		Random ran =new Random();
+		int com = ran.nextInt(3)+1;
+//		int com = scanner.nextInt();
+		System.out.println("컴퓨터의 선택"+ com);
+		String end = "";
+		if(use==com) {
+			end = "무승부";
+		}else if(use == 1) {
+			if(com == 2) {
+			end = "패배";
+			}else {
+			end = "승리";
+			}
+		}else if(use == 2) {
+			if(com == 1) {
+			end = "승리";
+			}else {
+			end = "패배";
+			}
+		}else if(use == 3) {
+			if(com == 1) {
+			end = "패배";
+			}else {
+			end = "승리";
+		}
+	}
+	System.out.println(end);	
+
+	}
+}
+	
